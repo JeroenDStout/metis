12c0029c486886573f953b24b7311c050ad47ee9
JeroenDStout <jeroenstout@gmail.com>
2024-09-01 15:17:58 +0200

    Add shadercross to sdl_3 cmake
    (cherry picked from commit 76211f6a8b9d271c070df5f8980bada7eeef8800)

--- cmake/generic/sdl_3.cmake
+++ cmake/generic/sdl_3.cmake
@@ -14,11 +14,26 @@ FetchContent_MakeAvailable(sdl_3)
 set_target_properties(SDL_uclibc SDL3_test SDL3-shared PROPERTIES
                       FOLDER "dependencies/sdl_3"
                       RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin")
+
+# Obtain SDL_gpu_shadercross
+# Single-file header to support shader compilation
+FetchContent_Declare(
+  sdl_gpu_shadercross
+  GIT_REPOSITORY https://github.com/flibitijibibo/SDL_gpu_shadercross.git
+  GIT_TAG        57e4a255125d26fc0fd91bc49336f1f56d77a8cc
+  GIT_PROGRESS   TRUE
+  SOURCE_DIR     $CACHE{FETCHCONTENT_BASE_DIR}/_deps/sdl_gpu_shadercross-src/shadercross
+)
+FetchContent_MakeAvailable(sdl_gpu_shadercross)
+file(GLOB sdl_gpu_shadercross_source_h ${sdl_gpu_shadercross_SOURCE_DIR}/*.h)
+add_library(sdl_gpu_shadercross INTERFACE ${sdl_gpu_shadercross_source_h})
+set_target_properties(sdl_gpu_shadercross PROPERTIES FOLDER "dependencies/sdl_3")
     
 # Config
 function(configure_sdl_target project_ref)
   message(STATUS "Configure SDL Target ${project_ref}")
   
   include_directories(${sdl_3_SOURCE_DIR}/include)
+  include_directories(${sdl_gpu_shadercross_SOURCE_DIR}/..)
   target_link_libraries(${project_ref} SDL3-shared)
 endfunction()
