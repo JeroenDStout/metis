3b48439a99de5a1c094412ac90c279b15d6e2402
JeroenDStout <jeroenstout@gmail.com>
2024-12-31 17:00:12 +0100

    Add douceurs

---
+++ cmake/generic/douceurs.cmake
@@ -0,0 +1,19 @@
+message(STATUS "Setup douceurs")
+
+include(FetchContent)
+
+# Obtain SDL3
+FetchContent_Declare(
+  douceurs
+  GIT_REPOSITORY https://github.com/JeroenDStout/douceurs.git
+  GIT_TAG        6c5abc110be1dd1992e55b0557265c25d9da4906
+  GIT_PROGRESS   TRUE
+)
+FetchContent_MakeAvailable(douceurs)
+    
+# Config
+function(configure_project_douceurs project_ref)
+  message(STATUS "Configure doucuers ${project_ref}")
+  
+  include_directories(${douceurs_SOURCE_DIR}/include)
+endfunction()
