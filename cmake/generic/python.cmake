7102b4d0ca09e039d1b403111d7bfbbe7aa2d325
Jeroen D Stout <jeroenstout@gmail.com>
2025-06-30 19:24:05 +0200

    Fix cmake errors

--- cmake/generic/python.cmake
+++ cmake/generic/python.cmake
@@ -3,6 +3,7 @@ message(STATUS "Setup Python3")
 include(ExternalProject)
 
 find_package(Python 3.8 COMPONENTS Interpreter Development.Module REQUIRED)
+message(STATUS "  Python Executable is: " ${Python_EXECUTABLE})
 
 #
 # Packages
